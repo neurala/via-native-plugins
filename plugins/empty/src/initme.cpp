c4c80788b9b444f6c0a466f812605b182706a46f
Florian Brenot <Ziink4@users.noreply.github.com>
2021-08-06 16:13:56 +0200

    Fix merge conflict

--- plugins/empty/src/initme.cpp
+++ plugins/empty/src/initme.cpp
@@ -37,7 +37,7 @@ initMe(NeuralaPluginManager* pluginManager, std::error_code* status)
 	auto& pm = *dynamic_cast<neurala::PluginRegistrar*>(pluginManager);
 	*status = pm.registerPlugin<neurala::plug::empty::CameraDiscoverer>("EmptyCameraDiscoverer",
 	                                                                    neurala::Version(1, 0));
-	if (*status != NeuralaPluginStatus::success)
+	if (*status != neurala::PluginStatus::success)
 	{
 		return nullptr;
 	}
