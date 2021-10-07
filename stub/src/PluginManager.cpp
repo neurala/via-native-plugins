b28cea4491d0f292603616e5fcabdf87a10ec632
Liviu Ivan <livan@neurala.com>
2021-10-07 13:26:44 +0300

    replace copyright statement

--- stub/src/PluginManager.cpp
+++ stub/src/PluginManager.cpp
@@ -1,40 +1,34 @@
 /*
- * This file is part of Neurala SDK.
- * Copyright Neurala Inc. 2013-2021. All rights reserved.
+ * Copyright (c) Neurala Inc. 2013-2021
  *
- * Except as expressly permitted in the accompanying License Agreement, if at all, (a) you shall
- * not license, sell, rent, lease, transfer, assign, distribute, display, host, outsource, disclose
- * or otherwise commercially exploit or make this source code available to any third party; (b) you
- * shall not modify, make derivative works of, disassemble, reverse compile or reverse engineer any
- * part of the SDK; (c) You shall not access the SDK in order to build a similar or competitive
- * product or service; (d) no part of the this source may be copied, reproduced, distributed,
- * republished, downloaded, displayed, posted or transmitted in any form or by any means, including
- * but not limited to electronic, mechanical, photocopying, recording or other means; and (e) any
- * future release, update, or other addition to functionality of the SDK shall be subject to the
- * terms of the accompanying License Agreement. You must reproduce, on all copies made by you or
- * for you, and must not remove, alter, or obscure in any way all proprietary rights notices
- * (including copyright notices) of Neurala Inc or its suppliers on or within the copies of the
- * SDK. Any sample code provided with the SDK and designated as such are for illustrative purposes
- * only and are not to be included in your applications.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:  The above copyright notice and this
+ * permission notice (including the next paragraph) shall be included in all copies or substantial
+ * portions of the Software.
  *
- * In cases when the accompanying License Agreement permits redistribution of this file, the above
- * notice shall be reproduced its entirety in every copy of a distributed version of this file.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#include <string>
 #include <algorithm>
 #include <mutex>
+#include <string>
 #include <unordered_map>
 
-#include "neurala/utils/Version.h"
-#include "neurala/plugin/PluginStatus.h"
-#include "neurala/plugin/PluginManager.h"
 #include "neurala/plugin/PluginArguments.h"
 #include "neurala/plugin/PluginErrorCallback.h"
+#include "neurala/plugin/PluginManager.h"
+#include "neurala/plugin/PluginStatus.h"
+#include "neurala/utils/Version.h"
 
 namespace neurala
 {
-
 // Classes registered by plugins and available for instantiation
 std::unordered_map<std::string, PluginRegistrar::ClassMetadata> m_classes;
 
@@ -54,9 +48,9 @@ registerClass(const char* name,
 	{
 		std::string className = name;
 
-		if (className.empty()
-		    || !std::all_of(className.cbegin(), className.cend(),
-		                    [](char c) { return std::isalnum(c) != 0 || c == '_'; }))
+		if (className.empty() || !std::all_of(className.cbegin(), className.cend(), [](char c) {
+			    return std::isalnum(c) != 0 || c == '_';
+		    }))
 		{
 			// empty name or name with anything except [a-zA-Z0-9\_] is not allowed
 			return make_error_code(PluginStatus::invalidName);
