9656390f5f5a1c12ba142df8cad55d2a8b8d4e82
Liviu Ivan <84592654+liviuivan-neurala@users.noreply.github.com>
2021-10-08 15:29:02 +0300

    replace copyright statement (#16)
    Co-authored-by: Liviu Ivan <livan@neurala.com>

--- plugins/websocket/test/Init.cpp
+++ plugins/websocket/test/Init.cpp
@@ -1,23 +1,19 @@
 /*
- * Copyright Neurala Inc. 2013-2021. All rights reserved.
+ * Copyright (c) Neurala Inc. 2013-2021
  *
- * Except as expressly permitted in the accompanying License Agreement, if at all, (a) you shall
- * not license, sell, rent, lease, transfer, assign, distribute, display, host, outsource, disclose
- * or otherwise commercially exploit or make this source code available to any third party; (b) you
- * shall not modify, make derivative works of, disassemble, reverse compile or reverse engineer any
- * part of the SDK; (c) You shall not access the SDK in order to build a similar or competitive
- * product or service; (d) no part of the this source may be copied, reproduced, distributed,
- * republished, downloaded, displayed, posted or transmitted in any form or by any means, including
- * but not limited to electronic, mechanical, photocopying, recording or other means; and (e) any
- * future release, update, or other addition to functionality of the SDK shall be subject to the
- * terms of the accompanying License Agreement. You must reproduce, on all copies made by you or
- * for you, and must not remove, alter, or obscure in any way all proprietary rights notices
- * (including copyright notices) of Neurala Inc or its suppliers on or within the copies of the
- * SDK. Any sample code provided with the SDK and designated as such are for illustrative purposes
- * only and are not to be included in your applications.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:  The above copyright notice and this
+ * permission notice (including the next paragraph) shall be included in all copies or substantial
+ * portions of the Software.
  *
- * In cases when the accompanying License Agreement permits redistribution of this file, the above
- * notice shall be reproduced its entirety in every copy of a distributed version of this file.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #define BOOST_TEST_MODULE Websocket
--- stub/include/neurala/meta/enum.h
+++ stub/include/neurala/meta/enum.h
@@ -1,24 +1,19 @@
 /*
- * This file is part of Neurala SDK.
- * Copyright Neurala Inc. 2013-2021. All rights reserved.
+ * Copyright (c) Neurala Inc. 2013-2021
  *
- * Except as expressly permitted in the accompanying License Agreement, if at all, (a) you shall
- * not license, sell, rent, lease, transfer, assign, distribute, display, host, outsource, disclose
- * or otherwise commercially exploit or make this source code available to any third party; (b) you
- * shall not modify, make derivative works of, disassemble, reverse compile or reverse engineer any
- * part of the SDK; (c) You shall not access the SDK in order to build a similar or competitive
- * product or service; (d) no part of the this source may be copied, reproduced, distributed,
- * republished, downloaded, displayed, posted or transmitted in any form or by any means, including
- * but not limited to electronic, mechanical, photocopying, recording or other means; and (e) any
- * future release, update, or other addition to functionality of the SDK shall be subject to the
- * terms of the accompanying License Agreement. You must reproduce, on all copies made by you or
- * for you, and must not remove, alter, or obscure in any way all proprietary rights notices
- * (including copyright notices) of Neurala Inc or its suppliers on or within the copies of the
- * SDK. Any sample code provided with the SDK and designated as such are for illustrative purposes
- * only and are not to be included in your applications.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:  The above copyright notice and this
+ * permission notice (including the next paragraph) shall be included in all copies or substantial
+ * portions of the Software.
  *
- * In cases when the accompanying License Agreement permits redistribution of this file, the above
- * notice shall be reproduced its entirety in every copy of a distributed version of this file.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #ifndef NEURALA_META_ENUM_H
@@ -51,7 +46,7 @@ namespace neurala
  */
 template<class Enum>
 struct MetaEnum
-{};
+{ };
 
 /**
  * @brief Registers @tparam Enum to support reflection via the associated @ref MetaEnum class.
@@ -67,8 +62,8 @@ struct MetaEnumRegister
 		EnumIntType value{};
 		const char* name{};
 
-		constexpr Entry(EnumIntType v, const char* n) noexcept : value{v}, name{n} {}
-		constexpr Entry(Enum v, const char* n) noexcept : value{EnumIntType(v)}, name{n} {}
+		constexpr Entry(EnumIntType v, const char* n) noexcept : value{v}, name{n} { }
+		constexpr Entry(Enum v, const char* n) noexcept : value{EnumIntType(v)}, name{n} { }
 
 		constexpr Enum enumValue() const noexcept { return static_cast<Enum>(value); }
 	};
@@ -83,12 +78,12 @@ struct MetaEnumRegister
  */
 template<class T, class = std17::void_t<>>
 struct hasMetaEnum : public std::false_type
-{};
+{ };
 
 // NOTE: must still use std17::void_t since CUDA uses this file
 template<class T>
 struct hasMetaEnum<T, std17::void_t<decltype(MetaEnum<T>::values)>> : public std::true_type
-{};
+{ };
 
 /**
  * @brief Creates a @c std::array with the enum values @p t, @p ts as created by
