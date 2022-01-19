780cc5753edf7145acecfe4776d1029c1f269c68
James Gerity <jgerity@neurala.com>
2022-01-19 12:21:38 -0500

    Update copyright year

--- stub/include/neurala/video/CameraDiscoverer.h
+++ stub/include/neurala/video/CameraDiscoverer.h
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) Neurala Inc. 2013-2021
+ * Copyright Neurala Inc. 2013-2022
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
