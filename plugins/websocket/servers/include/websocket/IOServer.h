d6fc83ebf148836ed95666d43c81a4a12fd081e1
RAYNAL JR <jraynal@users.noreply.github.com>
2024-05-24 10:36:47 -0400

    Updated copyright 2024 (#48)

--- plugins/websocket/servers/include/websocket/IOServer.h
+++ plugins/websocket/servers/include/websocket/IOServer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright Neurala Inc. 2013-2022
+ * Copyright Neurala Inc. 2013-2024
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
